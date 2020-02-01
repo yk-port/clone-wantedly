76cc21ef6290614812e8f489bf1be3bf287cfc45
tatsuya <yokokura.tatsuya@gmail.com>
2020-02-02 08:43:12 +0900

    feat(sidebar): Sidebarコンポーネントでstateを保有させる

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,6 +8,21 @@ import RecommendSearch from './sidebar/RecommendSearch';
 import SelectSearch from './sidebar/SelectSearch';
 
 class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      termsLists: [
+        { id: 1, terms: '未経験', tag: 'recommend', selected: true },
+        { id: 2, terms: 'フロントエンド', tag: 'recommend', selected: false },
+        { id: 3, terms: 'JavaScript', tag: 'recommend', selected: false },
+        { id: 4, terms: 'Rails', tag: 'recommend', selected: true },
+        { id: 5, terms: 'サーバーサイド', tag: 'recommend', selected: false },
+        { id: 6, terms: 'React', tag: 'recommend', selected: false },
+      ],
+    };
+  }
+
   render() {
     return(
       <aside className="aside">
@@ -16,8 +31,10 @@ class Sidebar extends React.Component {
           <div className="aside-contents">
             <SideSearch />
             <SeachFilter />
-            <SearchTerms />
-            <RecommendSearch />
+            <SearchTerms
+              termsLists={this.state.termsLists} />
+            <RecommendSearch
+              termsLists={this.state.termsLists} />
             <SelectSearch />
           </div>
         </div>
