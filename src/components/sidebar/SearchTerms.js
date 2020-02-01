c7767919f50258596d3458be60d3a011d5a7cc82
tatsuya <yokokura.tatsuya@gmail.com>
2020-02-01 21:05:17 +0900

    feat(sidebar): SearchTermsコンポーネントにstateを設定
    Parsing error: Unexpected token, expected ";"のエラーで先に進めず

--- src/components/sidebar/SearchTerms.js
+++ src/components/sidebar/SearchTerms.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchTerms extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      termsLists: [
+        { id: 1, terms: '未経験', tag: 'recommend', selected: false },
+        { id: 2, terms: 'フロントエンド', tag: 'recommend', selected: false },
+        { id: 3, terms: 'JavaScript', tag: 'recommend', selected: false },
+        { id: 4, terms: 'Rails', tag: 'recommend', selected: false },
+        { id: 5, terms: 'サーバーサイド', tag: 'recommend', selected: false },
+        { id: 6, terms: 'React', tag: 'recommend', selected: false },
+      ],
+    };
+
+    renderTerms() {
+      this.state.termsLists.forEach(termsList => {
+        if (termsList.selected) {
+          <div className="search-terms__selected">{termsList.term}</div>
+        }
+      });
+    }
+  }
+
   render() {
-    return(
+    return (
       <div className="search-terms" id="search-terms">
-        <div className="search-terms__selected">未経験</div>
-        <div className="search-terms__selected">フロントエンド</div>
+        {renderTerms()}
       </div>
     );
   }
+}
+
+SearchTerms.propTypes = {
+  renderTerms: PropTypes.func.isRequired,
 }
 
 export default SearchTerms;
