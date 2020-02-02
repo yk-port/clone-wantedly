362ed1bef03275d8eccc73261f3f01af9e1ad4ef
tatsuya <yokokura.tatsuya@gmail.com>
2020-02-02 16:47:32 +0900

    feat(sidebar): SidebarのstateをSelectSearchに渡す

--- src/components/sidebar/SelectSearch.js
+++ src/components/sidebar/SelectSearch.js
@@ -1,33 +1,46 @@
 import React from 'react';
 
 class SelectSearch extends React.Component {
+  handleClickAddTerms(id) {
+    this.props.onAddTerms(id)
+  }
+
+  renderTerms(tag) {
+    let searchTermsDom = this.props.termsLists.map(termsList => {
+      if (termsList.tag === tag && !termsList.selected) {
+        return (
+          <li className="select-search__list"
+              key={termsList.id}
+              onClick={() => this.handleClickAddTerms(termsList.id)} >
+            {termsList.terms}
+          </li>
+        );
+      }
+    });
+    return searchTermsDom;
+  }
+
   render() {
-    return(
+    return (
       <React.Fragment>
         <div className="select-search">
           <p className="select-search__heading">分野</p>
           <ul className="select-search__menu">
-            <li className="select-search__list">webエンジニア</li>
-            <li className="select-search__list">モバイルエンジニア</li>
-            <li className="select-search__list">インフラエンジニア</li>
+            {this.renderTerms('category')}
           </ul>
         </div>
 
         <div className="select-search">
           <p className="select-search__heading">地域</p>
           <ul className="select-search__menu">
-            <li className="select-search__list">東京</li>
-            <li className="select-search__list">関東</li>
-            <li className="select-search__list">リモート</li>
+            {this.renderTerms('area')}
           </ul>
         </div>
 
         <div className="select-search">
           <p className="select-search__heading">特徴</p>
           <ul className="select-search__menu">
-            <li className="select-search__list">TechCrunchに掲載実績あり</li>
-            <li className="select-search__list">海外進出している</li>
-            <li className="select-search__list">１億円以上の資金を調達済み</li>
+            {this.renderTerms('feature')}
           </ul>
         </div>
       </React.Fragment>
