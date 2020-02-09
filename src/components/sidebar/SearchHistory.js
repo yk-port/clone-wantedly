fc93d2957f9e1384fa8ff32f63322803ec4957fb
tatsuya <yokokura.tatsuya@gmail.com>
2020-02-09 20:17:32 +0900

    feat(search-history): マークアップ #12

---
+++ src/components/sidebar/SearchHistory.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+class SearchHistory extends React.Component {
+  render() {
+    return (
+      <div className="search-history">
+        <ul className="search-history__body">
+          <li className="search-history__list">
+            <div className="search-history__content">
+              <p className="search-history__keyword">React</p>
+              <p className="search-history__terms">
+                <span>React</span>
+                <span>中途採用</span>
+                <span>エンジニア</span>
+              </p>
+            </div>
+            <div className="search-history__icon">
+              <i class="far fa-star fa-lg"></i>
+            </div>
+          </li>
+          <li className="search-history__list">
+            <div className="search-history__content">
+              <p className="search-history__keyword">React</p>
+              <p className="search-history__terms">
+                <span>React</span>
+                <span>中途採用</span>
+                <span>エンジニア</span>
+              </p>
+            </div>
+            <div className="search-history__icon">
+              <i class="far fa-star fa-lg"></i>
+            </div>
+          </li>
+          <li className="search-history__list">
+            <div className="search-history__content">
+              <p className="search-history__keyword">React</p>
+              <p className="search-history__terms">
+                <span>React</span>
+                <span>中途採用</span>
+                <span>エンジニア</span>
+              </p>
+            </div>
+            <div className="search-history__icon">
+              <i class="far fa-star fa-lg"></i>
+            </div>
+          </li>
+          <li className="search-history__list">
+            <div className="search-history__content">
+              <p className="search-history__keyword">React</p>
+              <p className="search-history__terms">
+                <span>React</span>
+                <span>中途採用</span>
+                <span>エンジニア</span>
+              </p>
+            </div>
+            <div className="search-history__icon">
+              <i class="far fa-star fa-lg"></i>
+            </div>
+          </li>
+          <li className="search-history__list">
+            <div className="search-history__content">
+              <p className="search-history__keyword">React</p>
+              <p className="search-history__terms">
+                <span>React</span>
+                <span>中途採用</span>
+                <span>エンジニア</span>
+              </p>
+            </div>
+            <div className="search-history__icon">
+              <i class="far fa-star fa-lg"></i>
+            </div>
+          </li>
+          <li className="search-history__list">
+            <div className="search-history__content">
+              <p className="search-history__keyword">React</p>
+              <p className="search-history__terms">
+                <span>React</span>
+                <span>中途採用</span>
+                <span>エンジニア</span>
+              </p>
+            </div>
+            <div className="search-history__icon">
+              <i class="far fa-star fa-lg"></i>
+            </div>
+          </li>
+          <li className="search-history__list">
+            <div className="search-history__content">
+              <p className="search-history__keyword">React</p>
+              <p className="search-history__terms">
+                <span>React</span>
+                <span>中途採用</span>
+                <span>エンジニア</span>
+              </p>
+            </div>
+            <div className="search-history__icon">
+              <i class="far fa-star fa-lg"></i>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default SearchHistory;
--- src/components/sidebar/SideSearch.js
+++ src/components/sidebar/SideSearch.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchHistory from './SearchHistory';
 
 class SideSearch extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class SideSearch extends React.Component {
           <span className="side-search__title">検索条件</span>
           <span><i className="fas fa-clipboard"></i></span>
         </div>
+        <SearchHistory />
         <div className="side-search__input">
           <button type="submit" className="side-search__submit"><i className="fas fa-search"></i></button>
           <input type="text" className="side-search__box" placeholder="キーワードで検索" />
