54707620a3f9223882cdd427bdb500425138f816
tatsuya <yokokura.tatsuya@gmail.com>
2020-02-09 22:36:17 +0900

    feat(search-history): 検索履歴のダミーデータをstateに保有 #12
    mapを使ってループ処理で記述

--- src/components/sidebar/SearchHistory.js
+++ src/components/sidebar/SearchHistory.js
@@ -1,101 +1,44 @@
 import React from 'react';
 
 class SearchHistory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      histories: [
+        { label: 'React', offer: '中途採用', position: 'エンジニア' },
+        { label: 'Rails', offer: 'インターン', position: 'エンジニア' },
+        { label: 'マークアップ', offer: '業務委託', position: 'デザイナー' },
+        { label: 'AWS', offer: '中途採用', position: 'エンジニア' },
+        { label: 'カスタマーサクセス', offer: '中途採用', position: 'コーポレート' },
+        { label: 'UI/UX', offer: '業務委託', position: 'デザイナー' },
+        { label: 'スタートアップ', offer: '中途採用', position: 'ディレクター' },
+      ],
+    }
+  }
+
   render() {
     return (
       <div className="search-history">
         <ul className="search-history__body">
-          <li className="search-history__list">
-            <div className="search-history__content">
-              <p className="search-history__keyword">React</p>
-              <p className="search-history__terms">
-                <span>React</span>
-                <span>中途採用</span>
-                <span>エンジニア</span>
-              </p>
-            </div>
-            <div className="search-history__icon">
-              <i class="far fa-star fa-lg"></i>
-            </div>
-          </li>
-          <li className="search-history__list">
-            <div className="search-history__content">
-              <p className="search-history__keyword">React</p>
-              <p className="search-history__terms">
-                <span>React</span>
-                <span>中途採用</span>
-                <span>エンジニア</span>
-              </p>
-            </div>
-            <div className="search-history__icon">
-              <i class="far fa-star fa-lg"></i>
-            </div>
-          </li>
-          <li className="search-history__list">
-            <div className="search-history__content">
-              <p className="search-history__keyword">React</p>
-              <p className="search-history__terms">
-                <span>React</span>
-                <span>中途採用</span>
-                <span>エンジニア</span>
-              </p>
-            </div>
-            <div className="search-history__icon">
-              <i class="far fa-star fa-lg"></i>
-            </div>
-          </li>
-          <li className="search-history__list">
-            <div className="search-history__content">
-              <p className="search-history__keyword">React</p>
-              <p className="search-history__terms">
-                <span>React</span>
-                <span>中途採用</span>
-                <span>エンジニア</span>
-              </p>
-            </div>
-            <div className="search-history__icon">
-              <i class="far fa-star fa-lg"></i>
-            </div>
-          </li>
-          <li className="search-history__list">
-            <div className="search-history__content">
-              <p className="search-history__keyword">React</p>
-              <p className="search-history__terms">
-                <span>React</span>
-                <span>中途採用</span>
-                <span>エンジニア</span>
-              </p>
-            </div>
-            <div className="search-history__icon">
-              <i class="far fa-star fa-lg"></i>
-            </div>
-          </li>
-          <li className="search-history__list">
-            <div className="search-history__content">
-              <p className="search-history__keyword">React</p>
-              <p className="search-history__terms">
-                <span>React</span>
-                <span>中途採用</span>
-                <span>エンジニア</span>
-              </p>
-            </div>
-            <div className="search-history__icon">
-              <i class="far fa-star fa-lg"></i>
-            </div>
-          </li>
-          <li className="search-history__list">
-            <div className="search-history__content">
-              <p className="search-history__keyword">React</p>
-              <p className="search-history__terms">
-                <span>React</span>
-                <span>中途採用</span>
-                <span>エンジニア</span>
-              </p>
-            </div>
-            <div className="search-history__icon">
-              <i class="far fa-star fa-lg"></i>
-            </div>
-          </li>
+          {
+            this.state.histories.map(history => {
+              return (
+                <li className="search-history__list">
+                  <div className="search-history__content">
+                    <p className="search-history__label">{history.label}</p>
+                    <p className="search-history__terms">
+                      <span>{history.label}</span>
+                      <span>{history.offer}</span>
+                      <span>{history.position}</span>
+                    </p>
+                  </div>
+                  <div className="search-history__icon">
+                    <i class="far fa-star fa-lg"></i>
+                  </div>
+                </li>
+              );
+            })
+          }
         </ul>
       </div>
     );
