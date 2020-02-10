0e988bc10bbe1c03de05c5c3559b9ad472206288
tatsuya <yokokura.tatsuya@gmail.com>
2020-02-10 22:48:45 +0900

    refactor(SearchHistory): 検索リストを表示を関数化 #12

--- src/components/sidebar/SearchHistory.js
+++ src/components/sidebar/SearchHistory.js
@@ -5,15 +5,40 @@ class SearchHistory extends React.Component {
     super(props);
     this.state = {
       histories: [
-        { label: 'React', offer: '中途採用', position: 'エンジニア' },
-        { label: 'Rails', offer: 'インターン', position: 'エンジニア' },
-        { label: 'マークアップ', offer: '業務委託', position: 'デザイナー' },
-        { label: 'AWS', offer: '中途採用', position: 'エンジニア' },
-        { label: 'カスタマーサクセス', offer: '中途採用', position: 'コーポレート' },
-        { label: 'UI/UX', offer: '業務委託', position: 'デザイナー' },
-        { label: 'スタートアップ', offer: '中途採用', position: 'ディレクター' },
+        { id: 1, label: 'React', offer: '中途採用', position: 'エンジニア', flag: false },
+        { id: 2, label: 'Rails', offer: 'インターン', position: 'エンジニア', flag: false },
+        { id: 3, label: 'マークアップ', offer: '業務委託', position: 'デザイナー', flag: false },
+        { id: 4, label: 'AWS', offer: '中途採用', position: 'エンジニア', flag: false },
+        { id: 5, label: 'カスタマーサクセス', offer: '中途採用', position: 'コーポレート', flag: false },
+        { id: 6, label: 'UI/UX', offer: '業務委託', position: 'デザイナー', flag: false },
+        { id: 7, label: 'スタートアップ', offer: '中途採用', position: 'ディレクター', flag: false },
       ],
     }
+  }
+
+  handleClickListItem(id) {
+    console.log('handleClickListItem', id);
+  }
+
+  renderSearchHistoryList() {
+    let listDom = this.state.histories.map(history => (
+      <li className="search-history__list"
+          key={history.id}
+          onClick={() => this.handleClickListItem(history.id)} >
+        <div className="search-history__content">
+          <p className="search-history__label">{history.label}</p>
+          <p className="search-history__terms">
+            <span>{history.label}</span>
+            <span>{history.offer}</span>
+            <span>{history.position}</span>
+          </p>
+        </div>
+        <div className="search-history__icon">
+          <i className="far fa-star fa-lg"></i>
+        </div>
+      </li>
+    ));
+    return listDom;
   }
 
   render() {
@@ -21,25 +46,7 @@ class SearchHistory extends React.Component {
       return (
         <div className="search-history">
           <ul className="search-history__body">
-            {
-              this.state.histories.map(history => {
-                return (
-                  <li className="search-history__list">
-                    <div className="search-history__content">
-                      <p className="search-history__label">{history.label}</p>
-                      <p className="search-history__terms">
-                        <span>{history.label}</span>
-                        <span>{history.offer}</span>
-                        <span>{history.position}</span>
-                      </p>
-                    </div>
-                    <div className="search-history__icon">
-                      <i class="far fa-star fa-lg"></i>
-                    </div>
-                  </li>
-                );
-              })
-            }
+            {this.renderSearchHistoryList()}
           </ul>
         </div>
       );
