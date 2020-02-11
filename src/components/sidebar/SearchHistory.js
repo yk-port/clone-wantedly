bd811d5cc2a67d1d6ea656bd6b2a5966fe616d4e
tatsuya <yokokura.tatsuya@gmail.com>
2020-02-11 14:26:36 +0900

    feat(SearchHistory): ソートロジック途中実装

--- src/components/sidebar/SearchHistory.js
+++ src/components/sidebar/SearchHistory.js
@@ -5,19 +5,47 @@ class SearchHistory extends React.Component {
     super(props);
     this.state = {
       histories: [
-        { id: 1, label: 'React', offer: '中途採用', position: 'エンジニア', flag: false },
-        { id: 2, label: 'Rails', offer: 'インターン', position: 'エンジニア', flag: false },
-        { id: 3, label: 'マークアップ', offer: '業務委託', position: 'デザイナー', flag: false },
-        { id: 4, label: 'AWS', offer: '中途採用', position: 'エンジニア', flag: false },
-        { id: 5, label: 'カスタマーサクセス', offer: '中途採用', position: 'コーポレート', flag: false },
-        { id: 6, label: 'UI/UX', offer: '業務委託', position: 'デザイナー', flag: false },
-        { id: 7, label: 'スタートアップ', offer: '中途採用', position: 'ディレクター', flag: false },
+        { id: 1, label: 'React', offer: '中途採用', position: 'エンジニア', bookFlag: 0 },
+        { id: 2, label: 'Rails', offer: 'インターン', position: 'エンジニア', bookFlag: 0 },
+        { id: 3, label: 'マークアップ', offer: '業務委託', position: 'デザイナー', bookFlag: 0 },
+        { id: 4, label: 'AWS', offer: '中途採用', position: 'エンジニア', bookFlag: 0 },
+        { id: 5, label: 'カスタマーサクセス', offer: '中途採用', position: 'コーポレート', bookFlag: 0 },
+        { id: 6, label: 'UI/UX', offer: '業務委託', position: 'デザイナー', bookFlag: 0 },
+        { id: 7, label: 'スタートアップ', offer: '中途採用', position: 'ディレクター', bookFlag: 0 },
       ],
+      currentBookFlagNumber: 0,
     }
   }
 
   handleClickListItem(id) {
-    console.log('handleClickListItem', id);
+    let _state = Object.assign({}, this.state);
+    let listItem = _state.histories.find(history => {
+      return history.id === id;
+    });
+    let currentBookFlagNumber = _state.currentBookFlagNumber + 1;
+    if (listItem.bookFlag === 0) {
+      
+      console.log(currentBookFlagNumber);
+      
+      this.setState({ currentBookFlagNumber: 10 });
+
+      console.log(this.state.currentBookFlagNumber);
+
+    } else {
+      this.setState({ currentBookFlagNumber: 0 });
+    }
+    this.setState(_state);
+    console.log(_state);
+
+    this.sortForSearchHistoryList(_state);
+  }
+
+  sortForSearchHistoryList(_state) {
+    _state.histories.sort((a, b) => {
+      if (a.bookFlag < b.b) {
+        return 1;
+      }
+    });
   }
 
   renderSearchHistoryList() {
