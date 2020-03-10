840ea857067774714bed74fd74c9da5d8139f5b5
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-10 16:09:16 +0900

    build: build配下のファイルをignoreから削除

---
+++ build/precache-manifest.12bdba81c6f0f64cdc101ba6215b853c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663e43c1654d913bc140f11c38a184d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6966558f06ca691902ff",
+    "url": "/static/css/main.ac00f33f.chunk.css"
+  },
+  {
+    "revision": "52d800186cc446aa0b8e",
+    "url": "/static/js/2.dbdf28a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.dbdf28a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6966558f06ca691902ff",
+    "url": "/static/js/main.61e1e224.chunk.js"
+  },
+  {
+    "revision": "deaa83f1e15670cfa752",
+    "url": "/static/js/runtime-main.95b6c41f.js"
+  }
+]);
