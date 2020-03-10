a42b56c0a86f299847698afff9a7fc086aa7440c
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-10 16:29:59 +0900

    build: yarn add --dev gh-pages

---
+++ build/precache-manifest.7c250c3c41605cc2e742020adc83c096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446e945549e96168ed2d81358c2c94d7",
+    "url": "/clone-wantedly/index.html"
+  },
+  {
+    "revision": "f1d24a5931ac9aa0ce16",
+    "url": "/clone-wantedly/static/css/main.ac00f33f.chunk.css"
+  },
+  {
+    "revision": "52d800186cc446aa0b8e",
+    "url": "/clone-wantedly/static/js/2.dbdf28a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clone-wantedly/static/js/2.dbdf28a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1d24a5931ac9aa0ce16",
+    "url": "/clone-wantedly/static/js/main.a9a1946a.chunk.js"
+  },
+  {
+    "revision": "18b807ec84f9f2e412c6",
+    "url": "/clone-wantedly/static/js/runtime-main.2cca51cd.js"
+  }
+]);
